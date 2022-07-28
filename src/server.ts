6f7dcabac96192f28acb6ff0c34f2ac8f9226944
Will <rwps1234@gmail.com>
2022-07-28 13:17:58 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import { app } from ".";
+
+const PORT = process.env.PORT || 4000;
+
+const server = app.listen(PORT, () => {
+  console.log(`Servidor rodando com sucesso http://localhost:${PORT}/`);
+});
+
+process.on("SIGINT", () => {
+  server.close();
+  console.log("App Finalizado!");
+});
